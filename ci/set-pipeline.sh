4f8531058bb01c21e32852368c8965bd49eb1141
making <makingx@gmail.com>
2016-05-18 01:07:26 +0900

    Fix pipeline name

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo y | fly -t azr sp -p metflix-config-server -c pipeline.yml -l ../../credentials.yml
+echo y | fly -t azr sp -p metflix-eureka-server -c pipeline.yml -l ../../credentials.yml
